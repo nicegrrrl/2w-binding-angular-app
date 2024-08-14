fe74ad8f37588f620e574f957c49194a631b3f51
Alana Maia <alanaalvesmaia@gmail.com>
2024-08-14 14:03:52 -0300

    two-way binding with model (angular 17+)

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,14 +8,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  @Input({ required: true }) size!: { width: string; height: string };
-  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  // @Input({ required: true }) size!: { width: string; height: string };
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
   // this is NOT up to you!
   // you have to name the output like this (with Change at the endind) so Angular can know
   // that this input and output belongs together, is a two way bindable size property
 
+  // ⭐ angular 17 +
+  size = model.required<{ width: string; height: string }>();
+
   onReset() {
-    this.sizeChange.emit({
+    // this.sizeChange.emit({
+    //   width: '200',
+    //   height: '100',
+    // });
+
+    this.size.set({
       width: '200',
       height: '100',
     });
