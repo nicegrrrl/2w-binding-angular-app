99869c748b15fd45c6a49f29fd89e2ff890e2031
Alana Maia <alanaalvesmaia@gmail.com>
2024-08-14 13:50:58 -0300

    setting up custom two-way binding

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  // Todo: Implement custom two-way binding
+  @Input({ required: true }) size!: { width: string; height: string };
+  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  // this is NOT up to you!
+  // you have to name the output like this (with Change at the endind) so Angular can know
+  // that this input and output belongs together, is a two way bindable size property
 
   onReset() {
-    // ...
+    this.sizeChange.emit({
+      width: '200',
+      height: '100',
+    });
   }
 }
